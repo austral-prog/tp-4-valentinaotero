301e15177627c624b9038a77211550aadcff429a
valentinaotero <162721713+valentinaotero@users.noreply.github.com>
2024-04-12 11:44:18 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    year=int(input("Ingrese un año:"))
+    if (year%100==0 and year %400==0):
+        print("El año ingresado es bisiesto")
+    elif (year%4==0):
+        print("El año no es bisiesto")
+    else:
+        print("El año ingresado no es bisiesto")
