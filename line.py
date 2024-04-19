549a26435a7c6ea05c1638fb9eb119b80d1a4a2e
valentinaotero <162721713+valentinaotero@users.noreply.github.com>
2024-04-19 12:39:03 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,19 +1,19 @@
 import math
 def line():
 
-    variable_a=float(input("Ingrese el coeficiente A:"))
-    variable_b=float(input("Ingrese el coeficiente B:"))
-    variable_x1=float(input("Ingrese el coeficiente X1:"))
-    variable_x2=float(input("Ingrese el coeficiente X2:"))
-    print(f"El coeficiente A de su ecuacion de la recta es:{variable_a}")
-    print(f"El coeficiente B de su ecuacion de la recta es:{variable_b}")
-    print(f"El coeficiente X1 de su ecuacion de la recta es:{variable_x1}")
-    print(f"El coeficiente X2 de su ecuacion de la recta es:{variable_x2}")
+    variable_a=float(input("Ingrese el coeficiente A: "))
+    variable_b=float(input("Ingrese el coeficiente B: "))
+    variable_x1=float(input("Ingrese el coeficiente X1: "))
+    variable_x2=float(input("Ingrese el coeficiente X2: "))
+    print(f"El coeficiente A de su ecuación de la recta es: {variable_a}")
+    print(f"El coeficiente B de su ecuación de la recta es: {variable_b}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {variable_x1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {variable_x2}")
     print()
     print("Para la siguiente ecuación:")
-    print(f"\tY = {variable_a}x + {variable_b}")
-    variable_y1=((variable_a*variable_x1)+variable_b) 
-    variable_y2=((variable_a*variable_x2)+variable_b)
+    print(f"\tY = {variable_a}X + {variable_b}")
+    variable_y1=(float(variable_a*variable_x1)+variable_b) 
+    variable_y2=(float(variable_a*variable_x2)+variable_b)
     print()
     print("Dados los siguientes puntos:")
     print(f"\tP1 ({variable_x1}, {variable_y1})")
@@ -22,4 +22,4 @@ def line():
     P1=[variable_x1,variable_y1]
     P2=[variable_x2,variable_y2]
     variable_distance= math.dist(P1,P2)
-    print(f"La distancia entre ellos es:{variable_distance}")
+    print(f"La distancia entre ellos es: {variable_distance}")
